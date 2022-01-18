f6c40d8dd772df75d61ae9cac2ea0a513191b643
Josué Fidelis <jfon@cin.ufpe.br>
2022-01-18 10:45:42 -0300

    exercise 1.5

--- part1/exercise/part1-exercise/src/App.js
+++ part1/exercise/part1-exercise/src/App.js
@@ -37,27 +37,29 @@ function Total({ parts }) {
 
 
 function App() {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header course={course}></Header>
-      <Content parts={parts}></Content>
-      <Total parts={parts}
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}
         ></Total>
     </>
   )
