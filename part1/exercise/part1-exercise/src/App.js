6f670efb90c5cf2c4a287bd34b6a101b3fa2c02e
Josué Fidelis <jfon@cin.ufpe.br>
2022-01-18 10:42:48 -0300

    exercise 1.4

--- part1/exercise/part1-exercise/src/App.js
+++ part1/exercise/part1-exercise/src/App.js
@@ -1,5 +1,4 @@
 function Header(props) {
-  console.log(props);
   return (
     <>
       <h1>{props.course}</h1>
@@ -8,6 +7,7 @@ function Header(props) {
 }
 
 function Part(props) {
+  console.log(props);
   return (
     <>
       <p>
@@ -20,17 +20,17 @@ function Part(props) {
 function Content(props) {
   return (
     <>
-      <Part part={props.part1}></Part>
-      <Part part={props.part2}></Part>
-      <Part part={props.part3}></Part>
+      <Part part={props.parts[0]}></Part>
+      <Part part={props.parts[1]}></Part>
+      <Part part={props.parts[2]}></Part>
     </>
   );
 }
 
-function Total(props) {
+function Total({ parts }) {
   return (
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
     </>
   );
 }
@@ -38,29 +38,26 @@ function Total(props) {
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <>
       <Header course={course}></Header>
-      <Content part1={part1}
-        part2={part2}
-        part3={part3}
-        ></Content>
-      <Total exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+      <Content parts={parts}></Content>
+      <Total parts={parts}
         ></Total>
     </>
   )
