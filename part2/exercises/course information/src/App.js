18ddfe43bdc0b1319311c0fca1d73075b0b0d648
Josué Fidelis <jfon@cin.ufpe.br>
2022-01-20 10:53:44 -0300

    corrects exercise 2.1

--- part2/exercises/course information/src/App.js
+++ part2/exercises/course information/src/App.js
@@ -6,12 +6,11 @@ function Header(props) {
   );
 }
 
-function Part(props) {
-  console.log(props);
+function Part({ part }) {
   return (
     <>
       <p>
-        {props.part.name} {props.part.exercises}
+        {part.name} {part.exercises}
       </p>
     </>
   );
@@ -20,7 +19,7 @@ function Part(props) {
 function Content({ parts }) {
   return (
     <>
-      {parts.map(part => <Part part={part}></Part>)}
+      {parts.map(part => <Part key={part.id} part={part}></Part>)}
     </>
   );
 }
@@ -38,6 +37,8 @@ function Course({ course }) {
     <>
       <Header course={course.name}></Header>
       <Content parts={course.parts}></Content>
+      <Total parts={course.parts}
+        ></Total>
     </>
   );
 }
@@ -50,15 +51,18 @@ function App() {
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
