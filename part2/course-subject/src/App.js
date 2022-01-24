2bade661caf33400ca180d83c7049daa2d99d994
Josué Fidelis <jfon@cin.ufpe.br>
2022-01-24 10:21:00 -0300

    exercise 2.11

--- part2/course-subject/src/App.js
+++ part2/course-subject/src/App.js
@@ -1,10 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Note from './components/Note';
 
-function App(props) {
-  const [notes, setNotes] = useState(props.notes);
+function App() {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('...a new note.');
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log('effect');
+     axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promessa');
+        setNotes(response.data);
+      });
+  }, []);
+  console.log('render', notes.length, 'notes')
   
   const addNote = (event) => {
     event.preventDefault();
--- part2/exercises/phonebook/src/App.js
+++ part2/exercises/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ searchName, newSearchNameCallback }) => {
   return (
@@ -39,15 +40,19 @@ const Persons = ({ persons, searchName }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '2345678', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([]); 
+  const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchName, setSearchName] = useState('');
+
+  useEffect(() => {
+    console.log('teste');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      });
+  }, []);
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
