280b9926765ad8b78278fed408e6d669e4c418a9
Josué Fidelis <jfon@cin.ufpe.br>
2022-01-26 11:58:09 -0300

    update course-subject

--- part2/course-subject/src/App.js
+++ part2/course-subject/src/App.js
@@ -46,7 +46,13 @@ function App() {
     noteService.update(id, changedNote)
       .then((returnedNote) => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote));
-      });
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+    });
   };
   
   return (
